b2ec77d34e4b56849fe219f1c67dacd1af0b5c5f
Juan Manuel Aragon Paz <jmanuel@thinkanddev.com>
2022-06-20 10:31:06 -0300

    CorreccionDesafio3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //@ts-check
 import './App.css';
 import NavBar from './components/Navbar/NavBar'
-import ItemListContainer from './components/Navbar/ItemListContainer'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
   return (
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -12,7 +12,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import CartWidget from "./CartWidget";
+import CartWidget from "../CartWidget/CartWidget";
 import './NavBar.css'
 import {ReactComponent as MyLogo} from "../../assets/golf_2.svg";
 
