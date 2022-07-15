10bb2573d275acb76a8ed405d7b4555870e034fb
Juan Manuel Aragon Paz <jmanuel@thinkanddev.com>
2022-07-15 01:40:30 -0300

    feat spread operator test

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -6,8 +6,7 @@ function ItemList({ itemCategory }) {
   return (
     <div id="grid">
         {itemCategory.map((item) => 
-        <Item key={item.id} id={item.id} title={item.title} price={item.price} 
-        description={item.description} pictureUrl={item.pictureUrl} />
+        <Item key={item.id} {...item} />
         )}
     </div>
   );
